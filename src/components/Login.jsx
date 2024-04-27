84d27cc7256d6041274b09b72dad99b320931993
Tofigkh1 <143221445+Tofigkh1@users.noreply.github.com>
2024-04-27 09:06:02 +0400

    poppup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,34 +1,33 @@
 import React from "react";
-import "firebase/app";
 import { auth } from "../firebase";
-import firebase from 'firebase/app';
-
+import { GoogleAuthProvider, FacebookAuthProvider, signInWithPopup } from "firebase/auth";
 import { GoogleOutlined, FacebookOutlined } from '@ant-design/icons';
 
 const Login = () => {
+    const handleGoogleLogin = () => {
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider).catch(error => console.error(error));
+    };
+
+    const handleFacebookLogin = () => {
+        const provider = new FacebookAuthProvider();
+        signInWithPopup(auth, provider).catch(error => console.error(error));
+    };
 
     return (
         <div id="login-page">
             <div id="login-card">
-                <h2>Welcome to OnionChat !</h2>
-                <div
-                className="login-button google"
-                onClick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}
-                >
+                <h2>Welcome to OnionChat!</h2>
+                <div className="login-button google" onClick={handleGoogleLogin}>
                     <GoogleOutlined /> Sign In with Google
                 </div>
-                <br /> <br />
-
-                <div
-                className="login-button facebook"
-                onClick={() => auth.signInWithRedirect(new firebase.auth.FacebookAuthProvider())}
-                >
+                <br /><br />
+                <div className="login-button facebook" onClick={handleFacebookLogin}>
                     <FacebookOutlined /> Sign In with Facebook
                 </div>
             </div>
         </div>
     );
-
 }
 
 export default Login;
--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,21 @@
-import firebase from "firebase/app";
-import "firebase/auth";
+// Firebase modüllerini tek tek import etmek
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
-export const auth = firebase.initializeApp({
-    apiKey: "AIzaSyCF_GiJ2hT5u4v_Gi5DxWFURoun9iA28-M",
-    authDomain: "onionchat-7f127.firebaseapp.com",
-    projectId: "onionchat-7f127",
-    storageBucket: "onionchat-7f127.appspot.com",
-    messagingSenderId: "309019941005",
-    appId: "1:309019941005:web:9921ea9f25efdad6f7eac4"
-  }).auth();
+// Firebase yapılandırma detayları
+const firebaseConfig = {
+  apiKey: "AIzaSyCF_GiJ2hT5u4v_Gi5DxWFURoun9iA28-M",
+  authDomain: "onionchat-7f127.firebaseapp.com",
+  projectId: "onionchat-7f127",
+  storageBucket: "onionchat-7f127.appspot.com",
+  messagingSenderId: "309019941005",
+  appId: "1:309019941005:web:9921ea9f25efdad6f7eac4"
+};
 
+// Firebase uygulamasını başlat
+const app = initializeApp(firebaseConfig);
+
+// Auth servisini al
+const auth = getAuth(app);
+
+export { auth };
